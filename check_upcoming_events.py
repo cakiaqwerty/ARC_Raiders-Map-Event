c56e0e1755a9100fdfff90905cb0ad18fd320fbe
Cakir <muhdsyakira@gmail.com>
2026-04-03 21:54:40 +0800

    update 1

--- check_upcoming_events.py
+++ check_upcoming_events.py
@@ -16,12 +16,32 @@ Close Scrutiny, Cold Snap, Night Raid, ~
 Minor:
 Harvester, Matriach, Husk Graveyard, Bird City, ~
 """
-events_to_check = ["Bird City"]
+events_to_check = ["Locked Gate"]
+
+def convertTime(event,time):
+    return datetime.fromtimestamp(event[time] / 1000, tz=timezone.utc)
 
 for event in worldstate["data"]:
 
-    start = datetime.fromtimestamp(event["startTime"] / 1000, tz=timezone.utc)
-    end = datetime.fromtimestamp(event["endTime"] / 1000, tz=timezone.utc)
-
     if event["name"] in events_to_check:
-        print(event)
+
+        start = convertTime(event,"startTime")
+        end = convertTime(event,"endTime")
+
+        if start > now:
+            upcoming_events.append({
+                "name": event["name"],
+                "map": event["map"],
+                "startTime": start,
+                "endTime": end
+            })
+
+
+# filtered_events.sort(key=lambda x: x["start"])
+# filtered_events.sort(key=lambda x: (x["event"]["name"], x["start"]))
+print(f"Now: {now}")
+for eventReal in upcoming_events[:3]:
+    print(f"Condition: {eventReal["name"]}")
+    print(f"Map: {eventReal["map"]}")
+    print(f"Start Time: {eventReal["startTime"]}")
+    print(f"End Time: {eventReal["endTime"]}")
